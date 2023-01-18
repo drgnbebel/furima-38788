18235ce1018eced4795dcc41fc33d28ce6f42a19
drgnbebel <tko42470ef6@gmail.com>
2023-01-18 19:21:14 +0900

    商品情報編集機能メンターレビュー後の修正

--- app/controllers/frees_controller.rb
+++ app/controllers/frees_controller.rb
@@ -1,5 +1,6 @@
 class FreesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   
   def index
     @items = Item.order("created_at DESC")
@@ -44,5 +45,9 @@ class FreesController < ApplicationController
 
  def item_params
   params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+ end
+
+ def set_item
+  @item = Item.find(params[:id])
  end
 end
